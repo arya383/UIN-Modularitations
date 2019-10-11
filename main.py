57861953f4a70549e9ff86fa74e7e49921da93eb
arya383 <56154706+arya383@users.noreply.github.com>
2019-10-11 17:31:31 +0700

    fungsi ala sendiri

--- main.py
+++ main.py
@@ -15,5 +15,11 @@ def hitung_kecepatan(jarak,waktu):
 kecepatan = hitung_kecepatan(1000, 5 * 60)
 kecepatan = hitung_kecepatan(3000, 70 * 60)
 
+def Hitung_gaya(massa,percepatan):
+    gaya = massa * percepatan
+    print(f'massa = {massa / 1000}kg dikenai sebuah percepatan = {1000 / 3600}m/s')
+    print(f'sehingga gaya = {gaya} Newton')
+
+gaya = Hitung_gaya(30, 25)
 
 
